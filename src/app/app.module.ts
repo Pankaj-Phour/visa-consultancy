fe3334de4262a5c67b717443a16cc31e9700458c
Pankaj Phour <pankaj.phour70@gmail.com>
2024-08-05 09:54:10 +0530

    🚧 Added components to work on the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingComponent } from './landing/landing.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ContactComponent,
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
